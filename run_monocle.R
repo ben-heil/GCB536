776f1dd89a9e2b5827044aec058bcde7db4f9d5e
Ben Heil <ben.jer.heil@gmail.com>
2018-12-12 17:10:52 -0500

    Added a script to do locally linear embedding

--- run_monocle.R
+++ run_monocle.R
@@ -27,8 +27,7 @@ unsup_clustering_genes
 HSMM <- setOrderingFilter(HSMM, unsup_clustering_genes$gene_id)
 plot_ordering_genes(HSMM)
 
-HSMM <- reduceDimension(HSMM, max_components = 3, num_dim = 6,
-						                reduction_method = 'tSNE', verbose = T)
+HSMM <- reduceDimension(HSMM, max_components = 2,reduction_method = 'ICA', verbose = T)
 
 HSMM <- clusterCells(HSMM, num_clusters = 2)
 plot_cell_clusters(HSMM, 1, 2, color = "CellType")
